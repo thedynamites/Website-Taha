dbbe38397b9ecd840822940ad2b000ddc7fb51e2
Taha078 <45554555+Taha078@users.noreply.github.com>
2018-12-03 11:10:24 +0100

    Add files via upload

---
+++ website/home.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>vuurwerk</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <ul>
+  <li><a href="default.asp">Home</a></li>
+  <li><a href="news.asp">News</a></li>
+  <li><a href="contact.asp">Contact</a></li>
+  <li><a href="about.asp">About</a></li>
+</ul>
+
+<h1>Welkom</h1>
+<p>My first paragraph.</p>
+
+</body>
+</html>
